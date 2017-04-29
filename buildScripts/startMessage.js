47698ac8517ab6fe691b258d9b453256807666e6
dalmiro grañas <dalmiro graas>
2017-04-29 02:26:48 -0300

    end of Bundling chapter

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,6 @@
-/**
+//var chalk = require('chalk');
+import chalk from 'chalk';
+
+console.log(chalk.green('starting app in dev mode...'));/**
  * Created by dalmi on 27/04/2017.
  */
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-/**
- * Created by dalmi on 27/04/2017.
- */
+import './index.css';
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,0.00');
+debugger;
+console.log(`I would pay ${courseValue} for this awesome course!`);
